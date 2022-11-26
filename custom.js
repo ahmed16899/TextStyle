4eb4fd4555d9434ca6896ef39cfba5c8ec331fe5
ahmed16899 <97098478+ahmed16899@users.noreply.github.com>
2022-11-26 19:29:16 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,38 @@
+const par = document.getElementById('PAR');
+function ChangeFont(Family)
+{
+    par.style.fontFamily=Family;
+}
+function ChangeAlign(Align)
+{
+    par.style.textAlign=Align;
+}
+function ChangeHeight(height)
+{
+    par.style.lineHeight=height;
+}
+function ChangeLSpace(spacing)
+{
+    par.style.letterSpacing=spacing;
+}
+function ChangeIndent(Indent)
+{
+    par.style.textIndent=Indent;
+}
+function ChangeTransform(value)
+{
+    par.style.textTransform=value;
+}
+function ChangeDecorate(value)
+{
+    par.style.textDecoration=value;
+}
+function ChangeBorder(value)
+{
+    par.style.border=value;
+}
+
+function ChangeBorderColor(value)
+{
+    par.style.borderColor=value;
+}
